d17b0dd5b0abbf9cde3cdff0b5914d28961f016f
Magda Chudzik <kontakt@magdachudzik.pl>
2022-04-07 19:48:56 +0200

    feat: add global scss styles and button component

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'c-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
