324360256e79b0c2a44ae73e3881bbaf578b88fc
Magda Chudzik <kontakt@magdachudzik.pl>
2022-04-30 16:16:11 +0200

    feat: Add icon version and fix styles of ButtonComponent

--- src/app/components/ui-elements/button/button.component.ts
+++ src/app/components/ui-elements/button/button.component.ts
@@ -1,13 +1,21 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'c-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss'],
+    selector: 'c-button',
+    templateUrl: './button.component.html',
+    styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent {
-  @Input() click!: void;
-  @Input() variant!: string;
+    @Input() click!: void;
+    @Input() variant!: string;
 
-  constructor() {}
+    setClasses() {
+        return {
+            btn: true,
+            'btn--primary':
+                this.variant !== 'primary' && this.variant !== 'icon',
+            'btn--secondary': this.variant === 'secondary',
+            'btn--icon': this.variant === 'icon',
+        };
+    }
 }
