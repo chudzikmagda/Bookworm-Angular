5db602b6cdf1174c84cb4a3cf35ffe084dd59b80
Magda Chudzik <kontakt@magdachudzik.pl>
2022-10-25 22:01:14 +0200

    test: Add UTs for LogotypeComponent

--- src/app/components/ui-elements/logotype/logotype.component.spec.ts
+++ src/app/components/ui-elements/logotype/logotype.component.spec.ts
@@ -1,24 +1,44 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { LogotypeComponent } from './logotype.component';
 
 describe('LogotypeComponent', () => {
 	let component: LogotypeComponent;
 	let fixture: ComponentFixture<LogotypeComponent>;
 
-	beforeEach(async () => {
-		await TestBed.configureTestingModule({
+	beforeEach(() => {
+		TestBed.configureTestingModule({
 			declarations: [LogotypeComponent],
 		}).compileComponents();
-	});
 
-	beforeEach(() => {
 		fixture = TestBed.createComponent(LogotypeComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
 	});
 
-	it('should create', () => {
+	it('should create LogotypeComponent', () => {
 		expect(component).toBeTruthy();
+	});
+
+	it('should create small version of LogotypeComponent', () => {
+		component.version = 'small';
+		fixture.detectChanges();
+
+		const logotype: DebugElement = fixture.debugElement.query(
+			By.css('.logotype')
+		);
+
+		expect(logotype.classes['logotype']).toBeTrue();
+		expect(logotype.classes['logotype__small']).toBeTrue();
+	});
+
+	it('should create default version of LogotypeComponent', () => {
+		const logotype: DebugElement = fixture.debugElement.query(
+			By.css('.logotype')
+		);
+
+		expect(logotype.classes['logotype']).toBeTrue();
+		expect(logotype.classes['logotype__small']).not.toBeTrue();
 	});
 });
