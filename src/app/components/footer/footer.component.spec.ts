7088e9c42f23f10223c9f08ab9fabd20fac5a993
Magda Chudzik <kontakt@magdachudzik.pl>
2022-11-06 21:02:27 +0100

    test: Add UTs for FooterComponent

--- src/app/components/footer/footer.component.spec.ts
+++ src/app/components/footer/footer.component.spec.ts
@@ -1,24 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { ActionsService } from 'src/app/services/actions/actions.service';
+import { GoToTopComponent } from '../go-to-top/go-to-top.component';
+import { ButtonComponent } from '../ui-elements/button/button.component';
+import { LogotypeComponent } from '../ui-elements/logotype/logotype.component';
 import { FooterComponent } from './footer.component';
 
 describe('FooterComponent', () => {
 	let component: FooterComponent;
 	let fixture: ComponentFixture<FooterComponent>;
-
-	beforeEach(async () => {
-		await TestBed.configureTestingModule({
-			declarations: [FooterComponent],
-		}).compileComponents();
-	});
+	let fakeActionService: jasmine.SpyObj<ActionsService>;
 
 	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [
+				FooterComponent,
+				LogotypeComponent,
+				GoToTopComponent,
+				ButtonComponent,
+			],
+			providers: [
+				{
+					provide: ActionsService,
+					useValue: fakeActionService,
+				},
+			],
+		}).compileComponents();
+
 		fixture = TestBed.createComponent(FooterComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
 	});
 
-	it('should create', () => {
+	it('should create FooterComponent', () => {
 		expect(component).toBeTruthy();
+	});
+
+	it('should create footer with small logotype', () => {
+		const logotype: HTMLElement = fixture.debugElement.query(
+			By.directive(LogotypeComponent)
+		).nativeElement;
+
+		component.logotypeVersion = 'small';
+		fixture.detectChanges();
+
+		expect(logotype.hasAttribute('ng-reflect-version')).toBeTrue();
+		expect(logotype.getAttribute('ng-reflect-version')).toEqual('small');
 	});
 });
