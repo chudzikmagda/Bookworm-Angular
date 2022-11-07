bec9259db21b2a1750959b0320df09af244289d7
Magda Chudzik <kontakt@magdachudzik.pl>
2022-11-07 21:03:06 +0100

    test: Add Uts for GoToTopComponent

--- src/app/components/go-to-top/go-to-top.component.spec.ts
+++ src/app/components/go-to-top/go-to-top.component.spec.ts
@@ -1,25 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActionsService } from 'src/app/services/actions/actions.service';
+import { ButtonComponent } from '../ui-elements/button/button.component';
 import { GoToTopComponent } from './go-to-top.component';
 
 describe('GoToTopComponent', () => {
-  let component: GoToTopComponent;
-  let fixture: ComponentFixture<GoToTopComponent>;
+	let component: GoToTopComponent;
+	let fixture: ComponentFixture<GoToTopComponent>;
+	let fakeActionService: jasmine.SpyObj<ActionsService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ GoToTopComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(() => {
+		fakeActionService = jasmine.createSpyObj('ActionsService', [
+			'scrollToTheId',
+		]);
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GoToTopComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		TestBed.configureTestingModule({
+			declarations: [GoToTopComponent, ButtonComponent],
+			providers: [
+				{ provide: ActionsService, useValue: fakeActionService },
+			],
+		}).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		fixture = TestBed.createComponent(GoToTopComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create GoToTopComponent', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should scroll to top', () => {
+		window.scroll({
+			top: 100,
+			left: 0,
+		});
+		fixture.detectChanges();
+
+		component.goToTop();
+
+		expect(window.scrollY).toEqual(0);
+	});
 });
