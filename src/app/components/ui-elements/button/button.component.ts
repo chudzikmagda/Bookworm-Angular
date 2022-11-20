8bd429e012ff24385b9e21482a5675751a3d29f4
Magda Chudzik <kontakt@magdachudzik.pl>
2022-11-20 18:03:36 +0100

    feat: Add disabled input to ButtonComponent

--- src/app/components/ui-elements/button/button.component.ts
+++ src/app/components/ui-elements/button/button.component.ts
@@ -7,6 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class ButtonComponent {
 	@Input() click: void;
+	@Input() disabled: boolean;
 	@Input() variant:
 		| 'primary'
 		| 'secondary'
@@ -22,6 +23,7 @@ export class ButtonComponent {
 			'btn--primary-outline': this.variant === 'primary-outline',
 			'btn--secondary-outline': this.variant === 'secondary-outline',
 			'btn--icon': this.variant === 'icon',
+			'btn--disabled': this.disabled,
 		};
 	}
 }
