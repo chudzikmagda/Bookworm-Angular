0e55ba9576aa502900a4c73445f7f35213a336ec
Magda Chudzik <kontakt@magdachudzik.pl>
2022-12-30 22:13:27 +0100

    refactor: Move quote fetching from API to section container for QuoteComponent

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -1,28 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, Input } from '@angular/core';
 import { Quote } from 'src/app/models/models';
-import { ActionsService } from 'src/app/services/actions/actions.service';
-import { StateService } from 'src/app/services/state/state.service';
 
 @Component({
 	selector: 'c-quote',
 	templateUrl: './quote.component.html',
 	styleUrls: ['./quote.component.scss'],
 })
-export class QuoteComponent implements OnInit {
-	quote$: Observable<Quote>;
-
-	constructor(
-		private actionService: ActionsService,
-		private stateService: StateService
-	) {}
-
-	ngOnInit(): void {
-		this.loadQuote();
-	}
-
-	loadQuote(): void {
-		this.actionService.getSummaryQuoteFormApi('famous-quotes');
-		this.quote$ = this.stateService.getSummaryQuote();
-	}
+export class QuoteComponent {
+	@Input() quote: Quote;
 }
--- src/app/components/sections/summary/summary.component.ts
+++ src/app/components/sections/summary/summary.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
-import { SectionNames } from 'src/app/models/models';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Quote, SectionNames } from 'src/app/models/models';
+import { ActionsService } from 'src/app/services/actions/actions.service';
+import { StateService } from 'src/app/services/state/state.service';
 
 @Component({
 	selector: 'c-summary',
 	templateUrl: './summary.component.html',
 	styleUrls: ['./summary.component.scss'],
 })
-export class SummaryComponent {
+export class SummaryComponent implements OnInit {
+	QUOTE_TAG: string = 'famous-quotes';
 	sectionName: typeof SectionNames = SectionNames;
+	quote$: Observable<Quote | null>;
+
+	constructor(
+		private actionsService: ActionsService,
+		private stateService: StateService
+	) {}
+
+	ngOnInit(): void {
+		this.loadQuote();
+	}
+
+	loadQuote(): void {
+		this.actionsService.getSummaryQuoteFormApi(this.QUOTE_TAG);
+		this.quote$ = this.stateService.getSummaryQuote();
+	}
 }
