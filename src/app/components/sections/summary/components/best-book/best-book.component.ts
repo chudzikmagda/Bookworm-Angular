2d282d43dbf9332562db3cca0f3a42934c2c285f
Magda Chudzik <kontakt@magdachudzik.pl>
2023-01-16 23:11:56 +0100

    refactor: Move logic from BestBookComponent to it container

--- src/app/components/sections/summary/components/best-book/best-book.component.ts
+++ src/app/components/sections/summary/components/best-book/best-book.component.ts
@@ -1,44 +1,11 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subject, takeUntil, tap } from 'rxjs';
+import { Component, Input } from '@angular/core';
 import { BookData } from 'src/app/models/models';
-import { ActionsService } from 'src/app/services/actions/actions.service';
 
 @Component({
 	selector: 'c-best-book',
 	templateUrl: './best-book.component.html',
 	styleUrls: ['./best-book.component.scss'],
 })
-export class BestBookComponent implements OnInit, OnDestroy {
-	bestBook: BookData;
-
-	private books: BookData[];
-	private destroy$: Subject<boolean> = new Subject<boolean>();
-
-	constructor(private actionsService: ActionsService) {}
-
-	ngOnInit(): void {
-		this.loadBestBook();
-	}
-
-	private loadBestBook(): void {
-		this.actionsService
-			.getBooks()
-			.pipe(
-				takeUntil(this.destroy$),
-				tap((books: BookData[]) => (this.books = books)),
-				tap(() => {
-					this.books.length > 0 && this.setBestBook(this.books);
-				})
-			)
-			.subscribe();
-	}
-
-	private setBestBook(books: BookData[]): BookData {
-		return (this.bestBook = this.actionsService.bestBook(books));
-	}
-
-	ngOnDestroy(): void {
-		this.destroy$.next(true);
-		this.destroy$.unsubscribe();
-	}
+export class BestBookComponent {
+	@Input() bestBook: BookData;
 }
--- src/app/components/sections/summary/summary.component.ts
+++ src/app/components/sections/summary/summary.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { SectionNames, Quote } from 'src/app/models/models';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, skip, Subject, takeUntil, tap } from 'rxjs';
+import { SectionNames, Quote, BookData } from 'src/app/models/models';
 import { ActionsService } from 'src/app/services/actions/actions.service';
 import { StateService } from 'src/app/services/state/state.service';
 
@@ -9,10 +9,14 @@ import { StateService } from 'src/app/services/state/state.service';
 	templateUrl: './summary.component.html',
 	styleUrls: ['./summary.component.scss'],
 })
-export class SummaryComponent implements OnInit {
+export class SummaryComponent implements OnInit, OnDestroy {
 	QUOTE_TAG: string = 'famous-quotes';
 	sectionName: typeof SectionNames = SectionNames;
 	quote$: Observable<Quote | null>;
+	bestBook$: Observable<BookData[]>;
+	bestBook: BookData;
+
+	private onDestroy$: Subject<void> = new Subject<void>();
 
 	constructor(
 		private actionsService: ActionsService,
@@ -20,11 +24,40 @@ export class SummaryComponent implements OnInit {
 	) {}
 
 	ngOnInit(): void {
-		this.loadQuote();
+		this.loadData();
 	}
 
-	loadQuote(): void {
+	private loadData(): void {
+		this.loadQuote();
+		this.loadBooks();
+	}
+
+	private loadQuote(): void {
 		this.actionsService.getSummaryQuoteFormApi(this.QUOTE_TAG);
 		this.quote$ = this.stateService.getSummaryQuote();
+	}
+
+	private loadBooks(): void {
+		this.actionsService
+			.getBooks()
+			.pipe(
+				takeUntil(this.onDestroy$),
+				skip(1),
+				tap((books: BookData[]) => {
+					this.bestBook = this.setBestBook(books);
+				})
+			)
+			.subscribe();
+	}
+
+	private setBestBook(books: BookData[]) {
+		return books.reduce((prevBook: BookData, currBook: BookData) =>
+			prevBook.rating > currBook.rating ? prevBook : currBook
+		);
+	}
+
+	public ngOnDestroy(): void {
+		this.onDestroy$.next();
+		this.onDestroy$.complete();
 	}
 }
