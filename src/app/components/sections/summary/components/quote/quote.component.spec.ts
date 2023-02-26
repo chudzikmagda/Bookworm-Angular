035f8e563a13d6793d278db271060ba00b91d288
Magda Chudzik <kontakt@magdachudzik.pl>
2023-02-26 19:01:09 +0100

    test: Add UTs for QuoteComponent

--- src/app/components/sections/summary/components/quote/quote.component.spec.ts
+++ src/app/components/sections/summary/components/quote/quote.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Quote } from 'src/app/models/models';
 
 import { QuoteComponent } from './quote.component';
 
@@ -15,10 +17,40 @@ describe('QuoteComponent', () => {
 	beforeEach(() => {
 		fixture = TestBed.createComponent(QuoteComponent);
 		component = fixture.componentInstance;
+		component.quote = fakeQuote();
 		fixture.detectChanges();
 	});
 
-	it('should create', () => {
+	it('should create component', () => {
 		expect(component).toBeTruthy();
 	});
+
+	it('should display a quote content', () => {
+		const quoteContent: string = fixture.debugElement.query(
+			By.css('.quote__paragraph')
+		).nativeElement.innerText;
+
+		expect(quoteContent).toBe(fakeQuote().content);
+	});
+
+	it('should display an author of quote', () => {
+		const quoteAuthor: string = fixture.debugElement.query(
+			By.css('.quote__source')
+		).nativeElement.innerText;
+
+		expect(quoteAuthor).toBe('— ' + fakeQuote().author + ' —');
+	});
+
+	const fakeQuote = (): Quote => {
+		return {
+			author: 'fake author',
+			authorSlug: 'fake authorSlug',
+			content: 'Lorem ipsum',
+			dateAdded: 'fake dateAdded',
+			length: 1,
+			dateModified: 'fake dateModified',
+			tags: ['tag1', 'tag2'],
+			_id: 'fakeId',
+		};
+	};
 });
