895d609165dfada40793ba323c4cc096f6687ca0
Magda Chudzik <kontakt@magdachudzik.pl>
2023-04-04 22:20:16 +0200

    fix(ButtonComponent): Fix disabled attribiute

--- src/app/components/shared/ui-elements/button/button.component.ts
+++ src/app/components/shared/ui-elements/button/button.component.ts
@@ -6,15 +6,14 @@ import { Component, Input } from '@angular/core';
 	styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent {
-	@Input() click: void;
-	@Input() disabled: boolean;
-	@Input() class: string;
-	@Input() type: string;
-	@Input() variant: 'primary' | 'secondary' | 'primary-outline' | 'secondary-outline' | 'icon';
+	@Input() public click: void;
+	@Input() public disabled: boolean;
+	@Input() public class: string;
+	@Input() public type: string;
+	@Input() public variant: 'primary' | 'secondary' | 'primary-outline' | 'secondary-outline' | 'icon';
+	@Input() public size: 'large' | 'small';
 
-	@Input() size: 'large' | 'small';
-
-	setClasses() {
+	public setClasses(): { [key: string]: boolean } {
 		return {
 			btn: true,
 			'btn--primary': this.variant === 'primary',
