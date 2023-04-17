700a99ced50356330ad0b5822120298ba99a3059
Magda Chudzik <kontakt@magdachudzik.pl>
2023-04-17 21:50:28 +0200

    test(EditBookComponent): Add UTs

--- src/app/components/sections/books/components/edit-book/edit-book.component.spec.ts
+++ src/app/components/sections/books/components/edit-book/edit-book.component.spec.ts
@@ -1,25 +1,25 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MockComponents } from 'ng-mocks';
+import { of } from 'rxjs';
 import { BookFormComponent } from 'src/app/components/shared/book-form/book-form.component';
 import { BookFormService } from 'src/app/components/shared/book-form/services/book-form.service';
 import { DialogComponent } from 'src/app/components/shared/ui-elements/dialog/dialog.component';
 import { DialogService } from 'src/app/components/shared/ui-elements/dialog/services/dialog.service';
+import { BookData, BookFormData } from 'src/app/models/models';
 import { ActionsService } from 'src/app/services/actions/actions.service';
 import { EditBookComponent } from './edit-book.component';
-import { BookData } from 'src/app/models/models';
-import { of } from 'rxjs';
 
 describe('EditBookComponent', () => {
 	let component: EditBookComponent;
 	let fixture: ComponentFixture<EditBookComponent>;
-	let fakeDialogService: jasmine.SpyObj<DialogService>;
 	let fakeBookFormService: jasmine.SpyObj<BookFormService>;
 	let fakeActionsService: jasmine.SpyObj<ActionsService>;
 
 	beforeEach(() => {
-		fakeDialogService = jasmine.createSpyObj('DialogService', ['closeDialog']);
 		fakeBookFormService = jasmine.createSpyObj('BookFormService', ['setBookForm', 'getEditedBook$']);
 		fakeActionsService = jasmine.createSpyObj('ActionsService', ['updateBook']);
 
@@ -29,10 +29,7 @@ describe('EditBookComponent', () => {
 			declarations: [EditBookComponent, MockComponents(DialogComponent, BookFormComponent)],
 			imports: [BrowserAnimationsModule, ReactiveFormsModule],
 			providers: [
-				{
-					provide: DialogService,
-					useValue: fakeDialogService,
-				},
+				DialogService,
 				{
 					provide: BookFormService,
 					useValue: fakeBookFormService,
@@ -49,8 +46,28 @@ describe('EditBookComponent', () => {
 		fixture.detectChanges();
 	});
 
-	it('should create component', () => {
-		expect(component).toBeTruthy();
+	it('should set filled form on component init', () => {
+		const bookForm: DebugElement = fixture.debugElement.query(By.directive(BookFormComponent));
+		fakeBookFormService.getEditedBook$.and.returnValue(of([fakeEditedBook()]));
+
+		component.ngOnInit();
+
+		expect(fakeBookFormService.setBookForm).toHaveBeenCalledWith(fakeEditedBook());
+		expect(bookForm.componentInstance.bookFormTitle).toBe('Edit book');
+		expect(bookForm.componentInstance.submitButtonCopy).toBe('Save edited book');
+	});
+
+	it('should update edited book', () => {
+		const date: Date = new Date('2023-04-01T00:00:00');
+		jasmine.clock().mockDate(date);
+		fakeBookFormService.getEditedBook$.and.returnValue(of([fakeEditedBook()]));
+
+		component.ngOnInit();
+		component.editBook(fakeUpdatedBook());
+
+		expect(fakeActionsService.updateBook).toHaveBeenCalledWith(
+			expectedUpdatedBook() as unknown as BookData
+		);
 	});
 
 	const fakeBookList = (): BookData[] => {
@@ -63,7 +80,7 @@ describe('EditBookComponent', () => {
 				description: 'fakeDescription',
 				rating: 10,
 				date_add: '10 November 2022',
-				cover: 'fakeVover.jpg',
+				cover: 'fakeCover.jpg',
 			},
 			{
 				id: 2,
@@ -73,18 +90,46 @@ describe('EditBookComponent', () => {
 				description: 'fakeDescription2',
 				rating: 7,
 				date_add: '11 November 2022',
-				cover: 'fakeVover.jpg',
+				cover: 'fakeCover.jpg',
 			},
 			{
 				id: 3,
-				author: 'fakeAutho3',
+				author: 'fakeAuthor3',
 				title: 'fakeTitle3',
 				language: 'fakeLang',
 				description: 'fakeDescription3',
 				rating: 8,
 				date_add: '12 November 2022',
-				cover: 'fakeVover.jpg',
+				cover: 'fakeCover.jpg',
 			},
 		];
+	};
+
+	const fakeEditedBook = (): BookData => {
+		return {
+			id: 3,
+			author: 'fakeAuthor3',
+			title: 'fakeTitle3',
+			language: 'fakeLang',
+			description: 'fakeDescription3',
+			rating: 8,
+			date_add: '12 November 2022',
+			cover: 'fakeCover.jpg',
+		};
+	};
+
+	const fakeUpdatedBook = (): BookFormData => {
+		return {
+			author: 'fakeUpdatedAuthor3',
+			title: 'fakeTitle3',
+			language: 'fakeLang',
+			description: 'fakeDescription3',
+			rating: 8,
+			cover: 'fakeCover.jpg',
+		};
+	};
+
+	const expectedUpdatedBook = (): BookFormData => {
+		return { ...fakeEditedBook(), ...fakeUpdatedBook(), date_edit: '01/04/2023' } as BookFormData;
 	};
 });
