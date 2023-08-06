aa162f2450ccfb8bd439778e5e36f7938628105f
Magda Chudzik <kontakt@magdachudzik.pl>
2023-08-06 21:42:56 +0200

    fix(packages): fix security vulnerabilities

--- src/app/components/shared/ui-elements/dialog/services/dialog.service.spec.ts
+++ src/app/components/shared/ui-elements/dialog/services/dialog.service.spec.ts
@@ -1,16 +1,52 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DialogService } from './dialog.service';
+import { Router, Routes } from '@angular/router';
+import { AddNewBookComponent } from '../../../add-new-book/add-new-book.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { EventEmitter } from '@angular/core';
 
 describe('DialogService', () => {
 	let service: DialogService;
+	let fakeLocation: { back: jasmine.Spy };
+	let fakeRouter: { navigate: jasmine.Spy };
+
+	const testRoutes: Routes = [{ path: 'testPath', component: AddNewBookComponent }];
 
 	beforeEach(() => {
-		TestBed.configureTestingModule({});
+		fakeLocation = {
+			back: jasmine.createSpy('back'),
+		};
+
+		fakeRouter = {
+			navigate: jasmine.createSpy('navigate'),
+		};
+
+		TestBed.configureTestingModule({
+			imports: [RouterTestingModule.withRoutes(testRoutes)],
+			providers: [
+				{ provide: Router, useValue: fakeRouter },
+				{ provide: Location, useValue: fakeLocation },
+			],
+		});
+
 		service = TestBed.inject(DialogService);
 	});
 
-	it('should be created', () => {
-		expect(service).toBeTruthy();
+	it('should open a dialog', () => {
+		const path: string = 'testPath';
+
+		service.openDialog(path);
+
+		expect(fakeRouter.navigate).toHaveBeenCalledWith([path]);
+	});
+
+	fit('should close a dialog', () => {
+		const fakeVisibleChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+		service.closeDialog(true, fakeVisibleChange);
+
+		expect(fakeVisibleChange.emit).toHaveBeenCalledWith(true);
+		expect(fakeLocation.back).toHaveBeenCalled();
 	});
 });
